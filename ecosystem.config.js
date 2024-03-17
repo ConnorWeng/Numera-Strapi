4d04ec55fc502e5e631dc197d1d32357a5d2a3f4
Connor Weng <foamtea30@gmail.com>
2024-03-17 10:35:05 +0800

    Update

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: 'Numera-Strapi',
+      script: 'yarn',
+      args: 'start',
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+
+  deploy : {
+    production : {
+       "user" : "root",
+       "host" : ["106.14.190.250"],
+       "ref"  : "origin/main",
+       "repo" : "git@github.com:ConnorWeng/Numera-Strapi.git",
+       "path" : "/root/production/Numera-Strapi",
+       "post-deploy" : "yarn"
+    }
+  }
+};
