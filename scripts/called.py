392360b81f53a3690ce9e062c830237e783e94af
Connor Weng <foamtea30@gmail.com>
2024-03-24 00:57:00 +0800

    Fix cloud api url.

--- scripts/called.py
+++ scripts/called.py
@@ -6,7 +6,7 @@ import requests
 import json
 import time
 
-url = "http://106.14.190.250/api/calls"
+url = "http://106.14.190.250:1337/api/calls"
 API_TOKEN = "4db5bf6ba61e83a2188892e0b76dbdfb6b585c6db7daeebe82c1af8d9217980ded5d3a13bb57b868935778799ce2ed85190da3eee08dcf8370b8a34ef2ba8430649989577fb4ee19eb6e4a9ef4bf0d11dddacf3ff98436c9925c7af9acd210955934e57d29e0143ee8376fea14ab8ed4af8dfc8977f6320d302a4638c70a06e7"
 
 def report_call(callingNumber):
