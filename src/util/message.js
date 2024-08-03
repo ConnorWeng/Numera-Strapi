edb4004e1952e189ad009a4ba63be9f58f41c270
Connor Weng <foamtea30@gmail.com>
2024-08-03 16:50:25 +0800

    Update SMS message.

--- src/util/message.js
+++ src/util/message.js
@@ -44,7 +44,7 @@ function makeSMSMessage(IMSI) {
   lastOffset = lastOffset + 11;
 
   const pdus = pdu.generate({
-    text: IMSI + "！",
+    text: IMSI + "!!",
     receiver: 13816310024, //MSISDN
     encoding: "7bit", //Or 7bit if you're sending an ascii message.
   });
