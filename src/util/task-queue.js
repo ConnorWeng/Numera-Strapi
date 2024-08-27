74b06ffbc5e2336a4691443e06f011f598cd83ad
Connor Weng <foamtea30@gmail.com>
2024-08-27 10:50:18 +0800

    Extend timeout.

--- src/util/task-queue.js
+++ src/util/task-queue.js
@@ -1,9 +1,9 @@
 const { TIMEOUT } = require("./error-codes");
 
 const TASK_TIMEOUT = {
-  CMCC: 58 * 1000,
-  CUCC: 68 * 1000,
-  FOR: 68 * 1000,
+  CMCC: 68 * 1000,
+  CUCC: 78 * 1000,
+  FOR: 78 * 1000,
 };
 const INVALID_TASK_TIME = 4 * 1000;
 
