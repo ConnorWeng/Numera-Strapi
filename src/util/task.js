384429fb101469da73b494c7af3ce1844ffee97b
Connor Weng <foamtea30@gmail.com>
2024-09-06 00:18:45 +0800

    Default code 0.

--- src/util/task.js
+++ src/util/task.js
@@ -33,7 +33,7 @@ class TranslateTask {
     this.operator = null;
     this.dailyRemaining = null;
     this.done = false;
-    this.code = 999;
+    this.code = 0;
     this.derived = false;
     this.smsc = null;
     this.receiver = null;
