3842aae9623513435f4608eb4b4c0e5833917433
Connor Weng <foamtea30@gmail.com>
2025-02-09 18:20:13 +0800

    Refactor translate controller by moving error and result transformation logic to a new utility module.

--- src/api/translate/controllers/translate.js
+++ src/api/translate/controllers/translate.js
@@ -16,17 +16,7 @@ const {
   IMSI_NOT_ALLOWED,
   INVALID_SIGNATURE,
 } = require("../../../util/error-codes");
-const promClient = require("prom-client");
-
-const promCounter = new promClient.Counter({
-  name: "numera_translate_requests_total",
-  help: "Total number of translate requests",
-  labelNames: ["code", "status", "operator"],
-});
-
-const { service } = strapi.plugin("strapi-prometheus");
-const register = service("registry");
-register.registerMetric(promCounter);
+const { transformErrorTask, transformResult } = require("../../../util/common");
 
 /**
  * translate controller
@@ -39,59 +29,6 @@ const BORDER_REGEX = /^(40101|46003|46005|46011)[0-9]{9,10}$/;
 
 // load private key
 const privateKey = fs.readFileSync("private_key.pem", "utf8");
-
-const transformErrorTask = (isPythonClient, task, error) => {
-  strapi.log.info(
-    "translate error: " +
-      error.errorMessage +
-      ", task: " +
-      JSON.stringify(task),
-  );
-  promCounter.inc({
-    code: error.code,
-    status: "error",
-    operator: task.operator,
-  });
-  if (isPythonClient) {
-    return {
-      uid: task.uid,
-      imsi_phone: task.callingNumber,
-      sms_data: task.SMSData,
-      type: task.operator === "CMCC" ? 0 : 1,
-      timestamp: new Date().getTime() - task.createTime,
-      imsi: task.IMSI,
-      code: error.code,
-      quantity: task.dailyRemaining,
-      done: task.done,
-    };
-  } else {
-    throw new strapiUtils.errors.ValidationError(error.errorMessage);
-  }
-};
-
-const transformResult = (isPythonClient, task, updatedSMS) => {
-  let result = task;
-  promCounter.inc({
-    code: task.code,
-    status: task.code === 0 || task.code === 999 ? "success" : "error",
-    operator: task.operator,
-  });
-  if (isPythonClient) {
-    result = {
-      uid: task.uid,
-      imsi_phone: task.callingNumber,
-      sms_data: updatedSMS || task.SMSData,
-      type: task.operator === "CMCC" ? 0 : 1,
-      timestamp: new Date().getTime() - task.createTime,
-      imsi: task.IMSI,
-      code: task.code,
-      quantity: task.dailyRemaining,
-      done: task.done,
-    };
-  }
-  strapi.log.info(`Transformed result: ${JSON.stringify(result)}`);
-  return result;
-};
 
 const isTimestampValid = (decryptedMessage) => {
   const decryptedObject = JSON.parse(decryptedMessage);
---
+++ src/util/common.js
@@ -0,0 +1,70 @@
+const promClient = require("prom-client");
+const strapiUtils = require("@strapi/utils");
+
+const promCounter = new promClient.Counter({
+  name: "numera_translate_requests_total",
+  help: "Total number of translate requests",
+  labelNames: ["code", "status", "operator"],
+});
+
+const { service } = strapi.plugin("strapi-prometheus");
+const register = service("registry");
+register.registerMetric(promCounter);
+
+const transformErrorTask = (isPythonClient, task, error) => {
+  strapi.log.info(
+    "translate error: " +
+      error.errorMessage +
+      ", task: " +
+      JSON.stringify(task),
+  );
+  promCounter.inc({
+    code: error.code,
+    status: "error",
+    operator: task.operator,
+  });
+  if (isPythonClient) {
+    return {
+      uid: task.uid,
+      imsi_phone: task.callingNumber,
+      sms_data: task.SMSData,
+      type: task.operator === "CMCC" ? 0 : 1,
+      timestamp: new Date().getTime() - task.createTime,
+      imsi: task.IMSI,
+      code: error.code,
+      quantity: task.dailyRemaining,
+      done: task.done,
+    };
+  } else {
+    throw new strapiUtils.errors.ValidationError(error.errorMessage);
+  }
+};
+
+const transformResult = (isPythonClient, task, updatedSMS) => {
+  let result = task;
+  promCounter.inc({
+    code: task.code,
+    status: task.code === 0 || task.code === 999 ? "success" : "error",
+    operator: task.operator,
+  });
+  if (isPythonClient) {
+    result = {
+      uid: task.uid,
+      imsi_phone: task.callingNumber,
+      sms_data: updatedSMS || task.SMSData,
+      type: task.operator === "CMCC" ? 0 : 1,
+      timestamp: new Date().getTime() - task.createTime,
+      imsi: task.IMSI,
+      code: task.code,
+      quantity: task.dailyRemaining,
+      done: task.done,
+    };
+  }
+  strapi.log.info(`Transformed result: ${JSON.stringify(result)}`);
+  return result;
+};
+
+module.exports = {
+  transformErrorTask,
+  transformResult,
+};
