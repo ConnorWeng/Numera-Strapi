ad5eb363a61a1179f885d59a210dd15d080c67e4
Connor Weng <foamtea30@gmail.com>
2025-03-29 22:52:17 +0800

    Refactor error handling to mask IP addresses in log messages and update client version to 0.0.6.

--- scripts/client.py
+++ scripts/client.py
@@ -13,7 +13,8 @@ API_REQUEST_TYPE = {
     'LEGACY': 2
 }
 DEFAULT_PASSWORD = "wy1234"
-url = "http://106.14.190.250:1337/api"
+SERVER_IP = "106.14.190.250"
+url = "http://" + SERVER_IP + ":1337/api"
 fota = app_fota.new()
 
 # 华DTU板喂狗脚本. 必须存在, 否则模块会反复复位
@@ -41,6 +42,17 @@ logger = common.LogAdapter("Numera-Quec-Python-Client")
 logger.log_service.logtimeflag = True  # 设置LOG时间输出: True; False.
 logger.log_service.LEVEL = common.LOG_LV.DEBUG  # 设置LOG输出等级: DEBUG; INFO; CRITICAL; WARNING; ERROR.
 
+def mask_ip(log_message):
+    """
+    Replace IP address in the log message with *.*.*.*
+    """
+    return log_message.replace(SERVER_IP, '*.*.*.*')
+
+def raise_for_clean_error_message(e):
+    # 清理异常信息中的 IP 地址
+    error_message = str(e)
+    cleaned_message = mask_ip(error_message)  # 替换成通用信息
+    raise Exception(cleaned_message) from None
 
 class WatchDog:
     def __init__(self, max_count):
@@ -87,7 +99,7 @@ heartbeat_wdg = WatchDog(18)
 # 下面两个全局变量是必须有的，用户可以根据自己的实际项目修改下面两个全局变量的值，
 # 在执行用户代码前，会先打印这两个变量的值。
 CLIENT_NAME = "Numera Quec Python Client"
-CLIENT_VERSION = "0.0.5"
+CLIENT_VERSION = "0.0.6"
 checknet = checkNet.CheckNetwork(CLIENT_NAME, CLIENT_VERSION)
 
 q = Queue(1000)
@@ -183,8 +195,8 @@ def start_poll(uid):
                 break
             utime.sleep_ms(1000)
     except Exception as e:
-        logger.error('Poll Exception: {}'.format(e))
-        raise e
+        logger.error(mask_ip('Poll Exception: {}'.format(e)))
+        raise_for_clean_error_message(e)
 
 def poll(uid):
     global jwt_token
@@ -235,28 +247,32 @@ def process_queue():
                     check_upgrade()
                 utime.sleep_ms(1000)
     except Exception as e:
-        logger.error('Process Queue Exception: {}'.format(e))
+        logger.error(mask_ip('Process Queue Exception: {}'.format(e)))
 
 def check_upgrade():
     global jwt_token
     global last_check_upgrade_time
-    headers = {
-        'Content-Type': 'application/json'
-    }
-    logger.info('Ready to check upgrade')
-    response = request.get(url + '/devices/upgrade?clientVersion=' + CLIENT_VERSION, headers=headers, timeout=900)
-    response_data = response.json()
-    if response.status_code == 200:
-        last_check_upgrade_time = utime.time()
-        logger.info('Check upgrade result: {}, version: {}'.format(response_data['upgrade'], response_data['version']))
-        if response_data['upgrade']:
-            fota.download(response_data['url'], '/usr/client.mpy')
-            logger.info('Succeed to download the new version')
-            fota.set_update_flag()
-            logger.info('Upgrading...')
-            Power.powerRestart()
-    else:
-        logger.error('Check upgrade failed: {}'.format(response_data))
+    try:
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        logger.info('Ready to check upgrade')
+        response = request.get(url + '/devices/upgrade?clientVersion=' + CLIENT_VERSION, headers=headers, timeout=900)
+        response_data = response.json()
+        if response.status_code == 200:
+            last_check_upgrade_time = utime.time()
+            logger.info(mask_ip('Check upgrade result: {}, version: {}'.format(response_data['upgrade'], response_data['version'])))
+            if response_data['upgrade']:
+                fota.download(response_data['url'], '/usr/client.mpy')
+                logger.info('Succeed to download the new version')
+                fota.set_update_flag()
+                logger.info('Upgrading...')
+                Power.powerRestart()
+        else:
+            logger.error(mask_ip('Check upgrade failed: {}'.format(response_data)))
+    except Exception as e:
+        logger.error(mask_ip('Check upgrade Exception: {}'.format(e)))
+        raise_for_clean_error_message(e)
 
 def heartbeat():
     global net_status
@@ -283,7 +299,7 @@ def heartbeat():
             uart.write(ujson.dumps(request_json))
             utime.sleep(60)
     except Exception as e:
-        logger.error('Heartbeat Exception: {}'.format(e))
+        logger.error(mask_ip('Heartbeat Exception: {}'.format(e)))
 
 def format_status_report(data):
     if data['netStatus'] == 0:
@@ -309,13 +325,14 @@ def start():
         net_status = 1
         logger.error('Network connection failed.')
 
+    uart_wdg.start()
+    queue_wdg.start()
+    heartbeat_wdg.start()
+
     check_upgrade()
 
     uart = UART(UART.UART2, 115200, 8, 0, 1, 0)
     _thread.start_new_thread(uart_read, ())
     _thread.start_new_thread(process_queue, ())
     _thread.start_new_thread(heartbeat, ())
-    uart_wdg.start()
-    queue_wdg.start()
-    heartbeat_wdg.start()
     logger.info('UART2 initialized.')
